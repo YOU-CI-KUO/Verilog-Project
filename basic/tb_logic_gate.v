c7650d0ff465ce0e02cf646baaec75340f1cc298
郭宥騏 <yuki980012@gmail.com>
2024-08-09 16:42:31 +0800

    Update tb_logic_gate.v

--- basic/tb_logic_gate.v
+++ basic/tb_logic_gate.v
@@ -25,10 +25,10 @@ module tb();
   initial 
     begin
        a = 1'b0; b = 1'b0;
-	  #5;a = 1'b0; b = 1'b1;
-	  #5;a = 1'b1; b = 1'b0;
-	  #5;a = 1'b1; b = 1'b1;
-    #5;
+       #5;a = 1'b0; b = 1'b1;
+       #5;a = 1'b1; b = 1'b0;
+       #5;a = 1'b1; b = 1'b1;
+       #5;
     end
   
   initial
