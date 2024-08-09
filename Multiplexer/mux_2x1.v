1e39dbfa849478b05d498c9c01688f605b75a429
郭宥騏 <yuki980012@gmail.com>
2024-08-09 17:11:31 +0800

    Update mux_2x1.v

--- Multiplexer/mux_2x1.v
+++ Multiplexer/mux_2x1.v
@@ -4,10 +4,11 @@ module mux_2x1(
   output reg [7:0]Y
 );
   
-  always @ (A, B, SEL)
+  always @ (*) begin
     case (SEL)
       1'b0: Y <= A; // if sel is 0 select A input
       1'b1: Y <= B; // if sel is 1 select B input
       default: Y <= 8'b0;
     endcase
+  end
 endmodule
